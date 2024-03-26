1d9dd3a8b9154372f82fd03bc34f058ae6295489
Heitor José <heitorscalon@gmail.com>
2024-03-26 20:55:01 -0300

    feat: ensure routes are in its own package and implements router interface

---
+++ routes/routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"net/http"
+	"rinha-backend/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func Routers() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Get("/clientes/{id}/extrato", handlers.GetTransactionsHandler)
+	mux.Post("/cadastrar", handlers.CreateClientHandler)
+	mux.Post("/clientes/{id}/transacoes", handlers.CreateNewTransactionHandler)
+
+	return mux
+}
