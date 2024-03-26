2e045ed4b8f1cd3f7ad0ae903baed8b869fd38cb
Heitor José <heitorscalon@gmail.com>
2024-03-26 20:58:45 -0300

    feat: ensure main.go works correctly after changes

--- main.go
+++ main.go
@@ -5,57 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"rinha-backend/data"
+	"rinha-backend/config"
 	"time"
 
 	_ "github.com/jackc/pgconn"
-	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func ConfigPGX() *pgxpool.Config {
-	const defaultMaxConns = int32(5)
-	const defaultMinConns = int32(0)
-	const defaultMaxConnLifetime = time.Hour
-	const defaultMaxConnIdleTime = time.Minute * 30
-	const defaultHealthCheckPeriod = time.Minute
-	const defaultConnectTimeout = time.Second * 5
-
-	DATABASE_URL := os.Getenv("DSN")
-
-	dbConfig, err := pgxpool.ParseConfig(DATABASE_URL)
-	if err != nil {
-		log.Fatal("Failed to create a config, error: ", err)
-	}
-
-	dbConfig.MaxConns = defaultMaxConns
-	dbConfig.MinConns = defaultMinConns
-	dbConfig.MaxConnLifetime = defaultMaxConnLifetime
-	dbConfig.MaxConnIdleTime = defaultMaxConnIdleTime
-	dbConfig.HealthCheckPeriod = defaultHealthCheckPeriod
-	dbConfig.ConnConfig.ConnectTimeout = defaultConnectTimeout
-
-	dbConfig.BeforeAcquire = func(ctx context.Context, c *pgx.Conn) bool {
-		log.Println("Before acquiring the connection pool to the database!!")
-		return true
-	}
-
-	dbConfig.AfterRelease = func(c *pgx.Conn) bool {
-		log.Println("After releasing the connection pool to the database!!")
-		return true
-	}
-
-	return dbConfig
-}
-
 const port = "8080"
-
-type Config struct {
-	DB     *pgxpool.Pool
-	Models data.Models
-}
 
 func main() {
 	log.Println("Starting authentication service")
@@ -66,14 +24,11 @@ func main() {
 
 	defer pool.Close()
 
-	app := Config{
-		DB:     pool,
-		Models: data.New(pool),
-	}
+	app := config.NewConfig(pool)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
-		Handler: app.routes(),
+		Handler: app.Routers,
 	}
 	err = srv.ListenAndServe()
 
@@ -89,7 +44,7 @@ func connectToDB() (*pgxpool.Pool, error) {
 	for i := 0; i < 10; i++ {
 		log.Printf("Connecting to Postgres, attempt %d\n", i+1)
 
-		pool, err = pgxpool.ConnectConfig(context.Background(), ConfigPGX())
+		pool, err = pgxpool.ConnectConfig(context.Background(), config.ConfigPGX())
 		if err != nil {
 			log.Printf("Failed to connect to Postgres: %v\n", err)
 			time.Sleep(2 * time.Second)
